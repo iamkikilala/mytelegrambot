62ab0b0dd5e164ca95afa7457d975d3291942748
iamkikilala <121472689+iamkikilala@users.noreply.github.com>
2025-04-04 23:41:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -207,5 +207,8 @@ async def main():
     )
 
 if __name__ == '__main__':
-    asyncio.run(main())
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+git add bot.py
+git commit -m "fix: avoid RuntimeError - event loop already running"
+git push
